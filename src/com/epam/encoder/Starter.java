f21e3cd8b5fa35fb9ed4f5a31cb65144ac85c6b8
DianaSeitbaeva <diana.seytbaeva@mail.ru>
2020-03-07 03:59:24 +0600

    Encoder homework

--- src/com/epam/encoder/Starter.java
+++ src/com/epam/encoder/Starter.java
@@ -1,12 +1,32 @@
 package com.epam.encoder;
 
-public class Starter {
-    public static void main(String[] args) {
-        String x = "sos sos";
-        String x1 = "... --- ... // ... --- ...";
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
-        System.out.println(EncodeKey.convertEng(x));
-        System.out.println(EncodeKey.convertMorse(x1));
+public class Starter {
+    public static void main(String[] args) throws IOException {
+        String x = null;
+        try (FileInputStream text = new FileInputStream(new File("C:/Users/Zim Evanse/IdeaProjects/Encoder/src/com/epam/encoder/eng.txt"))) {
+            Scanner sc = new Scanner(text);
+            x = sc.nextLine();
+        }
+        String x1 = null;
+        try (FileInputStream text1 = new FileInputStream(new File ("C:/Users/Zim Evanse/IdeaProjects/Encoder/src/com/epam/encoder/morse.txt"))){
+            Scanner sc1 = new Scanner(text1);
+            x1 = sc1.nextLine();
+            }
+        catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        }
+        System.out.println("Convert to morse");
+        System.out.println(Converter.convertEng(x));
+        System.out.println("Convert to english");
+        System.out.println(Converter.convertMorse(x1));
+
+
     }
 }
 
